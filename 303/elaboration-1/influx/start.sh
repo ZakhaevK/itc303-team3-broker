e9efa15e380bf6832bfbc14c7b030e4385098c74
Desmodena <sara_mezzomo@csu.edu.au>
2023-05-11 19:54:31 +1000

    timestamp troubleshooting

--- 303/elaboration-1/influx/start.sh
+++ 303/elaboration-1/influx/start.sh
@@ -3,7 +3,7 @@
 echo 'starting docker container'
 docker run -d --rm --name influxdb -p 8086:8086 \
 	-e DOCKER_INFLUXDB_INIT_MODE=setup \
-	-e DOCKER_INFLUXDB_INIT_USERNAME=user \
+	-e DOCKER_INFLUXDB_INIT_USERNAME=Desmodena \
 	-e DOCKER_INFLUXDB_INIT_PASSWORD=password \
 	-e DOCKER_INFLUXDB_INIT_ORG=ITC303 \
 	-e DOCKER_INFLUXDB_INIT_BUCKET=DPI \
