d744e7016b76313115a62f56f0f5d3959ddf4a8e
cal <14177650+cufffs@users.noreply.github.com>
2023-05-11 20:56:58 +1000

    some progress

--- 303/elaboration-1/influx/influx2.py
+++ 303/elaboration-1/influx/influx2.py
@@ -7,6 +7,7 @@ import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WriteOptions, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 import json
+import math
 
 #variables for connecting to the db
 token = os.environ.get("INFLUXDB_TOKEN")
@@ -22,12 +23,26 @@ bucket="DPI"
 #create a writeAPI client
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
+"""
+def parse_input_file() -> None:
+  f = open("../../docs/sample_messages", "r")
+  l = f.readlines()
+  for line in l:
+    return msg_parser(json.loads(line))
+
+def msg_parser(msg: str) -> None:
+    ret = f'DPI,p_uid={msg["p_uid"]},l_uid={msg["l_uid"]} '
+    ret += ",".join([f"{i['name']}={i['value']}" for i in msg["timeseries"]])
+    ret += " " + str(time.mktime(parser.parse(msg['timestamp']).timetuple()))
+    return ret
+"""
+
 #define a function to parse json message and convert to influxdb line protocol
 def json_to_line_protocol(json_msg):
   parsed_msg = json.loads(json_msg)
   l_uid = parsed_msg['l_uid']
   p_uid = parsed_msg['p_uid']
-  timestamp = (time.mktime(parser.parse(parsed_msg['timestamp']).timetuple())*1000)
+  timestamp = int(time.mktime(parser.parse(parsed_msg['timestamp']).timetuple())*1000)
   #ts_datetime = datetime.fromisoformat(timestamp)
   #ts_unix = int(ts_datetime.timestamp()*1000)
   measurements = parsed_msg['timeseries']
@@ -35,6 +50,8 @@ def json_to_line_protocol(json_msg):
   for measurement in measurements:
     measurement_name = measurement['name']
     measurement_value = measurement['value']
+    if measurement_value == "NaN":
+        continue
     line = f"{measurement_name},l_uid={l_uid},p_uid={p_uid} value={measurement_value} {timestamp}"
     lines.append(line)
   return "\n".join(lines)
@@ -44,10 +61,11 @@ def json_to_line_protocol(json_msg):
 with open("../../docs/sample_messages", "r") as f:
   json_msgs = f.readlines()
   lines = [json_to_line_protocol(msg) for msg in json_msgs]
-
+#lines = parse_input_file()
+print(lines)
 #write lines
-write_api = client.write_api(write_options=WriteOptions(batch_size=100, flush_interval=500, jitter_interval=400, retry_interval = 2500))
-write_api.write(bucket=bucket, org=org, record=lines, write_precision="ms")
+write_api = client.write_api(write_options=WriteOptions(batch_size=100, flush_interval=5000, jitter_interval=400, retry_interval = 2500))
+write_api.write(bucket=bucket, org=org, record=lines, write_precision="ns")
 
 #example json msg
 #
