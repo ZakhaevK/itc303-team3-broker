d44eb36bd2e4e59b08af90d51d1f5c273f7f8308
cal <14177650+cufffs@users.noreply.github.com>
2023-05-28 21:26:10 +1000

    various fixes to pass testing

--- src/python/tsdb/IoTaDecoder.py
+++ src/python/tsdb/IoTaDecoder.py
@@ -2,6 +2,7 @@ from questdb.ingress import Sender, IngressError
 import requests
 import sys
 from dateutil import parser
+import json
 
 bucket_name = "dpi"
 tsdb_host_name = "quest"
@@ -11,23 +12,33 @@ def parse_json_msg(msg: str) -> None:
     """
     This function is the main message parser, it will convert IoTa format to something TSDB can insert
     """
-    syms = {"p_uid" : f'{msg["p_uid"]}', "l_uid" : f'{msg["l_uid"]}'}
-    #syms = {}
-    cols = {clean(item['name']):item['value'] for item in msg['timeseries']}
-    #cols['p_uid'] = msg['p_uid']
-    #cols['l_uid'] = msg['l_uid']
-    timestamp = parser.parse(msg['timestamp'])
-    insert_line_protocol(syms, cols, timestamp)
+
+    #try convert to json object if it is not
+    if not isinstance(msg, dict):
+        try:
+            msg = json.loads(msg)
+        except Exception as e:
+            sys.stderr.write(f'error: wrong format {e}\n')
+            return None, None, None
+
+    try:
+        syms = {"p_uid" : f'{msg["p_uid"]}', "l_uid" : f'{msg["l_uid"]}'}
+        cols = {clean_names(item['name']):item['value'] for item in msg['timeseries']}
+        timestamp = parser.parse(msg['timestamp'])
+        return syms, cols, timestamp
+    except Exception as e:
+        sys.stderr.write(f'error: unable to parse {e}\n')
+        return None, None, None
 
 
-def insert_line_protocol(syms: str, cols: str, timestamp: str) -> None:
+def insert_line_protocol(syms: str, cols: str, timestamp: str, hostname=tsdb_host_name, port=tsdb_port, bucket=bucket_name) -> None:
     """
     This function will insert a message via line protocol to questdb
     """
     try:
-        with Sender(tsdb_host_name,tsdb_port) as sender:
+        with Sender(hostname,port) as sender:
             sender.row(
-                bucket_name,
+                bucket,
                 symbols=syms,
                 columns=cols,
                 at=timestamp
@@ -37,20 +48,13 @@ def insert_line_protocol(syms: str, cols: str, timestamp: str) -> None:
         sys.stderr.write(f'error: {e}\n')
 
 
-def clean(msg: str) -> str:
-    return msg.replace('(','').replace(')','').replace('/','')
-    return msg.replace('(','').replace(')',"").replace('/','')
-
-def get_all_inserts() -> str:
+def clean_names(msg: str) -> str:
     """
-    This function is helper, to simply return all data in db
-    REMOVE ME AFTER TESTING
+    Table and column names must not contain any of the forbidden characters: 
+    \n \r ? , : " ' \\ / \0 ) ( + * ~ %
+
+    Additionally, table name must not start or end with the . character. 
+    Column name must not contain . -
     """
-
-    return requests.get(
-        f'http://{tsdb_host_name}:{tsdb_port}/exec',
-        {
-            'query':f'{bucket_name} ORDER BY l_uid;'
-        }
-    ).text
-
+    translation_table = str.maketrans("", "", "\n\r?,:\"'\\/).(+*~%.-")
+    return msg.translate(translation_table)
