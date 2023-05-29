1e3bc9c361d55a39af46296df8874cbc99c3a67a
cal <14177650+cufffs@users.noreply.github.com>
2023-05-29 16:11:37 +1000

    moved lts reader to iotadecoder

--- src/python/tsdb/IoTaDecoder.py
+++ src/python/tsdb/IoTaDecoder.py
@@ -1,16 +1,132 @@
-from questdb.ingress import Sender, IngressError
-import requests
-import sys
-from dateutil import parser
-import json
+"""
+This program receives logical device timeseries messages and decodes message 
+into time series data and inserts into QuestDB time series database
 
+skeleton is based off LTSReader.py
+"""
+import asyncio, json, logging, signal, sys
+
+from pika.exchange_type import ExchangeType
+import api.client.RabbitMQ as mq
+import BrokerConstants
+import util.LoggingUtil as lu
+import api.client.DAO as dao
+
+from questdb.ingress import Sender, IngressError
+from dateutil import parser
+
+#TODO: move these to .env file
 bucket_name = "dpi"
 tsdb_host_name = "quest"
 tsdb_port = 9009
 
-def parse_json_msg(msg: str) -> None:
+#TODO: move these to main end check it doesn't break anything
+rx_channel = None
+mq_client = None
+finish = False
+
+
+def sigterm_handler(sig_no, stack_frame) -> None:
     """
-    This function is the main message parser, it will convert IoTa format to something TSDB can insert
+    Handle SIGTERM from docker by closing the mq and db connections and setting a
+    flag to tell the main loop to exit.
+    """
+    global finish, mq_client
+
+    logging.info(f'Caught signal {signal.strsignal(sig_no)}, setting finish to True')
+    finish = True
+    mq_client.stop()
+    dao.stop()
+
+
+async def main():
+    """
+    Initiate the connection to RabbitMQ and then idle until asked to stop.
+
+    Because the messages from RabbitMQ arrive via async processing this function
+    has nothing to do after starting connection.
+    """
+    global mq_client, rx_channel, finish
+
+    logging.info('===============================================================')
+    logging.info('               STARTING IoTaDecoder (TSDB ENCODER)             ')
+    logging.info('===============================================================')
+
+    # The routing key is ignored by fanout exchanges so it does not need to be a constant.
+    # Change the queue name. This code should change to use a server generated queue name.
+    rx_channel = mq.RxChannel(
+        BrokerConstants.LOGICAL_TIMESERIES_EXCHANGE_NAME, exchange_type=ExchangeType.fanout, 
+        queue_name='ltsreader_logical_msg_queue', on_message=on_message, routing_key='logical_timeseries'
+    )
+    mq_client = mq.RabbitMQConnection(channels=[rx_channel])
+    asyncio.create_task(mq_client.connect())
+
+    while not rx_channel.is_open:
+        await asyncio.sleep(0)
+
+    while not finish:
+        await asyncio.sleep(2)
+
+    while not mq_client.stopped:
+        await asyncio.sleep(1)
+
+
+def on_message(channel, method, properties, body):
+    """
+    This function is called when a message arrives from RabbitMQ.
+    """
+
+
+    """
+    dao.get_current_device_mapping(luid..)
+    ==
+    pd=PhysicalDevice(
+        uid=1, source_name='ttn', name='Test Sensor 1', location=Location(lat=-34.3372, long=508.6245),
+        last_seen=None, source_ids={'app_id': 'ttn-app-id-1', 'dev_id': 'ttn-device-id-1', 'dev_eui': 'ttn-dev-eui-1'},
+        properties={'description': 'Sample Test Device Properties'})
+    ld=LogicalDevice(
+        uid=1, name='Test Sensor 1', location=Location(lat=-34.3372, long=508.6245),
+        last_seen=None, properties={'description': 'Sample Test Device Properties'})
+        start_time=datetime.datetime(2023, 5, 29, 13, 49, 33, 240101,
+        tzinfo=datetime.timezone(datetime.timedelta(seconds=36000)))
+        end_time=None
+    """
+
+    global rx_channel, finish
+
+    delivery_tag = method.delivery_tag
+
+    # If the finish flag is set, reject the message so RabbitMQ will re-queue it
+    # and return early.
+    if finish:
+        rx_channel._channel.basic_reject(delivery_tag)
+        return
+
+    try:
+        msg = json.loads(body)
+        lu.cid_logger.info(f'Received Message: {msg}', extra=msg)
+
+        #TODO:add checks and create uid for puid + luid
+        #logging.info(dao.get_current_device_mapping(msg['l_uid']))
+
+        syms, cols, timestamp = parse_json_msg(msg)
+        if syms is not None:
+            insert_line_protocol(syms, cols, timestamp)
+            rx_channel._channel.basic_ack(delivery_tag)
+            return
+    except Exception as e:
+        lu.cid_logger.error(f'Unable to process LTS message: {e}')
+        rx_channel.channel.basic_reject(delivery_tag)
+        return
+
+    # This tells RabbitMQ the message is handled and can be deleted from the queue
+    #rx_channel._channel.basic_ack(delivery_tag)
+    rx_channel.channel.basic_reject(delivery_tag)
+
+def parse_json_msg(msg: str) -> str:
+    """
+    This function is the main message parser, 
+    it will convert IoTa format to something TSDB can insert
     """
 
     #try convert to json object if it is not
@@ -31,7 +147,10 @@ def parse_json_msg(msg: str) -> None:
         return None, None, None
 
 
-def insert_line_protocol(syms: str, cols: str, timestamp: str, hostname=tsdb_host_name, port=tsdb_port, bucket=bucket_name) -> int:
+def insert_line_protocol(syms: str, cols: str, timestamp: str, 
+                         hostname=tsdb_host_name, port=tsdb_port, 
+                         bucket=bucket_name
+                         ) -> int:
     """
     This function will insert a message via line protocol to questdb
     """
@@ -60,3 +179,16 @@ def clean_names(msg: str) -> str:
     """
     translation_table = str.maketrans("", "", "\n\r?,:\"'\\/).(+*~%.-")
     return msg.translate(translation_table)
+
+
+if __name__ == '__main__':
+    # Docker sends SIGTERM to tell the process the container is stopping so set
+    # a handler to catch the signal and initiate an orderly shutdown.
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+    # Ctrl-C sends SIGINT, handle it the same way.
+    signal.signal(signal.SIGINT, sigterm_handler)
+
+    # Does not return until SIGTERM is received.
+    asyncio.run(main())
+    logging.info('Exiting.')
