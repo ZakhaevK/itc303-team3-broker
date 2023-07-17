774687dabbd0d83d98e4670e8013431b08f8939a
RishCSU <rsing107@postoffice.csu.edu.au>
2023-07-17 18:07:43 +1000

    made some chanes and made the tests more robust

--- src/python/promdelivery/test/test_speed.py
+++ src/python/promdelivery/test/test_speed.py
@@ -32,11 +32,16 @@ def generate_message():
     msg = "{\"broker_correlation_id\": " + str(corr_id) + \
     ", \"p_uid\": " + str(p_uid) + \
     ", \"l_uid\": " + str (l_uid) + \
-    "\"timestamp\": \"2023-01-30T06:21:56Z\"," +  \
-    "\"timeseries\": [\{\"name\": \"battery (v)\"," + \
-    "\"value\": " + str(value) + "\}]\}"
+    ", \"timestamp\": \"2023-01-30T06:21:56Z\", " +  \
+    "\"timeseries\": [{\"name\": \"battery (v)\"," + \
+    "\"value\": " + str(value) + "}]}"
 
     return msg, value
+
+def extract_value(message):
+    json_dict = json.loads(message)
+    return json_dict["timeseries"][0]["value"]
+
 
 def get_db_response():
     host = "localhost"
@@ -49,25 +54,42 @@ def get_db_response():
 
 def single_insert_time_test():
     msg, val = generate_message()
+    print(msg)
     start_time = time.time()
     send_message(msg)
+    while True:
+        response = get_db_response()
+        test_val = response["data"]["result"][0]["value"][1]
+        if val == (test_val):
+            break 
     end_time = time.time()
     total_time = end_time - start_time
     print("Time for single insert: " + str(total_time))
 
 def multi_insert_time_test():
     time.sleep(2)
+    messages = []
+    with open("msgs", "r") as msg_file:
+        for index, line in enumerate(msg_file):
+            messages.append(line)
+            if index == 999:
+                break
+
     start_time = time.time()
     for i in range(1000):
-        msg, val = generate_message()
+        # msg, val = generate_message()
+        msg = messages[i]
+        val = extract_value(msg)
         send_message(msg)      
-        time.sleep(0.3)  
-        # while True:
-        #     response = get_db_response()
-        #     test_val = response["data"]["result"][0]["value"][1]
-        #     print(i)
-        #     if str(val) == test_val:
-        #         break               
+        # time.sleep(0.3)  
+        # print("progressing!")
+        while True:
+            response = get_db_response()
+            test_val = response["data"]["result"][0]["value"][1]
+            # print(str(i) + ", " + str(val) + ", " + str(test_val))
+            # time.sleep(1)
+            if (val) == eval(test_val):
+                break               
     end_time = time.time()
     total_time = end_time - start_time
     print("Time for multi (1000) insert: " + str(total_time))
