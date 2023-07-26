c9d36b3c595a2aaacffafa21727829d487f410bd
RishCSU <rsing107@postoffice.csu.edu.au>
2023-07-26 17:28:16 +1000

    Revert "Merge branch 'influx' of https://github.com/ZakhaevK/itc303-team3-broker into influx"
    This reverts commit 648dc31d4cc0c08e76ba37a01e4b17dac80503aa, reversing
    changes made to 56ad59a618399e8852ae858b392ed729c2faf413.

--- src/python/webserver/server.py
+++
@@ -1,8 +0,0 @@
-import http.server
-import socketserver
-
-handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", 8080), handler) as httpd:
-    print("Server started at localhost:" + str(8080))
-    httpd.serve_forever()
